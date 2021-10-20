3d56ba625fbeb4d4acf11dec874ff3576cd2213f
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-10-20 12:57:09 -0400

    search field plus function returns results in console

--- main.js
+++ main.js
@@ -1,36 +1,25 @@
 console.log("JS hooked up")
 
-
 const songs = document.getElementById("songs-list")
 
 const form = document.querySelector('#search-form')
 
 const submitButton = document.getElementById('submit-search')
 
+let searchText = document.getElementById('search-text')
 
-// document.getElementById('submit-search').addEventListener('click', (e) => {
-//     console.log(searchText)
-//     function listSongs(searchText, data) {
-//         const url = 'https://proxy-itunes-api.glitch.me/search?media=music&entity=song&term='
-//         fetch(url + 'kiss')
-//             .then((res) => res.json())
-//             .then((data) => {
-//                 console.log(data);
-//             })
-//         }
-    
-// })
+const url = 'https://proxy-itunes-api.glitch.me/search?media=music&entity=song&term='
 
+document.getElementById('submit-search').addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(searchText.value)
+    listSongs(searchText)
+})
 
 
 // listSongs function works, will console log data
-
-
-    let searchText = ('prince')
-
-    function listSongs(searchText, data) {
-    const url = 'https://proxy-itunes-api.glitch.me/search?media=music&entity=song&term='
-    fetch(url + "prince")
+    function listSongs(data) {
+    fetch(url + searchText.value)
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
@@ -40,21 +29,8 @@ const submitButton = document.getElementById('submit-search')
 listSongs()
 
 
-// let searchText = document.getElementById('searchText').value;
-// fetch(`url + ${searchText} + '&entity=song'`)
-// .then((res) => res.json())
-// .then((data) => {
-//     console.log(data);
-//     // renderSongs(data)
-// })
 
 
-// function renderSongs(data) {
-//     const li = document.createElement('li')
-//     li.id = songObj.id
-//     renderSongAttributes(li, data)
-//     songs.appendChild(li)
-// }
 
 
 // // what goes in the {} on line 45????
