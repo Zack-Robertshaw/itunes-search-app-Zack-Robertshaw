89b5f79eace2df4553aef63efebbc38498b93ea8
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-10-20 16:13:25 -0400

    songs populate but don't go away on new search

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("JS hooked up")
 
+
 const songs = document.getElementById("songs-list")
 
 const form = document.querySelector('#search-form')
@@ -8,23 +9,43 @@ const submitButton = document.getElementById('submit-search')
 
 let searchText = document.getElementById('search-text')
 
-const url = 'https://proxy-itunes-api.glitch.me/search?media=music&entity=song&term='
+const url = 'https://proxy-itunes-api.glitch.me/search?media=music&entity=song&limit=10&term='
 
 document.getElementById('submit-search').addEventListener('click', (e) => {
     e.preventDefault()
     console.log(searchText.value)
     listSongs(searchText)
+    form.reset()
 })
 
 
 // listSongs function works, will console log data
-    function listSongs(data) {
+    function listSongs() {
     fetch(url + searchText.value)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
+            // console.log(data.results) 
+            for (let item of data.results) {
+                // root.appendChild(data.results)
+                console.log(item.artistName)
+                songs.innerHTML +=
+                `<div class="card" >
+                <img src=${item.artworkUrl100}>
+                <li>${item.artistName}</li>
+                <li>${item.trackName}</li>
+                <li>${item.collectionName}</li>
+                <audio controls src=${item.previewUrl}></audio>
+                </div>
+
+
+
+                `
+            }
         })
     }
+
+
+
 
 listSongs()
 
