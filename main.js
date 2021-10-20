ad7dcb77f1434ebedf3349f343a563700d4905a2
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-10-20 09:43:36 -0400

    function is hotwired, returns data to console log

--- main.js
+++ main.js
@@ -1,46 +1,65 @@
 console.log("JS hooked up")
 
-const url = 'https://proxy-itunes-api.glitch.me/search?term='
 
 const songs = document.getElementById("songs-list")
 
 const form = document.querySelector('#search-form')
 
-// SUBMIT BUTTON DOESN'T WORK
+const submitButton = document.getElementById('submit-search')
 
-// const submitButton = document.getElementById('submit-search')
-// form.addEventListener('search', (e) => {
-//     e.preventDefault
-//     searchText = document.getElementById('search-text').value
-//         createSong(songText)
-//         form.reset()
-//     })
 
+// document.getElementById('submit-search').addEventListener('click', (e) => {
+//     console.log(searchText)
+//     function listSongs(searchText, data) {
+//         const url = 'https://proxy-itunes-api.glitch.me/search?media=music&entity=song&term='
+//         fetch(url + 'kiss')
+//             .then((res) => res.json())
+//             .then((data) => {
+//                 console.log(data);
+//             })
+//         }
+    
+// })
 
 
 
 // listSongs function works, will console log data
-// pretty sure renderSongs works too
-// renderSongAttributes line 45 artistName is undefined
-function listSongs() {
-    fetch(url+'prince'+'&entity=song')
+
+
+    let searchText = ('prince')
+
+    function listSongs(searchText, data) {
+    const url = 'https://proxy-itunes-api.glitch.me/search?media=music&entity=song&term='
+    fetch(url + "prince")
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
-            renderSongs(data)
         })
     }
 
-
-function renderSongs(songObj) {
-const li = document.createElement('li')
-li.id = songObj.id
-renderSongAttributes(li, songObj)
-songs.appendChild(li)
-}
-// what goes in the {} on line 45????
-function renderSongAttributes(li, songObj) {
-    li.innerHTML = `<span>${results.id.artistName}</span>`
-}
-
 listSongs()
+
+
+// let searchText = document.getElementById('searchText').value;
+// fetch(`url + ${searchText} + '&entity=song'`)
+// .then((res) => res.json())
+// .then((data) => {
+//     console.log(data);
+//     // renderSongs(data)
+// })
+
+
+// function renderSongs(data) {
+//     const li = document.createElement('li')
+//     li.id = songObj.id
+//     renderSongAttributes(li, data)
+//     songs.appendChild(li)
+// }
+
+
+// // what goes in the {} on line 45????
+// function renderSongAttributes(li, songObj) {
+//     li.innerHTML = `<span>${results.id.artistName}</span>`
+// }
+
+// 
